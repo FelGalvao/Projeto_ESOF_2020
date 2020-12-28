6e5d3b05671867d43789e647b901c83d715ac76d
marcelorocha876 <marcelorocha876.mr@gmail.com>
2020-12-28 14:24:09 +0000

    c

--- projetoesof/src/main/java/pt/ufp/inf/esof/projetoesof/models/Projeto.java
+++ projetoesof/src/main/java/pt/ufp/inf/esof/projetoesof/models/Projeto.java
@@ -3,8 +3,24 @@ package pt.ufp.inf.esof.projetoesof.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
 @Getter
 @Setter
 public class Projeto {
+    @Id
+    private Long id;
     private String nome;
+    @ManyToOne
+    private Cliente cliente;
+
+    public Projeto(String nome, Cliente cliente) {
+        this.nome = nome;
+        this.cliente = cliente;
+    }
+
+
+    public Projeto() {
+    }
 }
